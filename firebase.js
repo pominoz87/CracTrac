78d051070a6067dac550ec70936262ac939e1065
pominoz87 <ca.stallard@gmail.com>
2025-02-24 22:29:40 +0800

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -4,19 +4,19 @@ const firebaseConfig = {
   apiKey: "AIzaSyCeZ-Fzpswh-d8lGmgPex3SvS6sSC9vH6Q",
   authDomain: "cractrac-3629a.firebaseapp.com",
   projectId: "cractrac-3629a",
-  storageBucket: "cractrac-3629a.firebasestorage.app",
+  storageBucket: "cractrac-3629a.appspot.com",
   messagingSenderId: "603257438362",
-  appId: "1:603257438362:web:ee551d7ff493c32165df23";
- 
+  appId: "1:603257438362:web:ee551d7ff493c32165df23"
 };
+
 firebase.initializeApp(firebaseConfig);
 const dbFirestore = firebase.firestore();
 
 dbFirestore.enablePersistence()
   .catch((err) => {
-    if (err.code == 'failed-precondition') {
+    if (err.code === 'failed-precondition') {
       console.error("Multiple tabs open, persistence can only be enabled in one tab at a time.");
-    } else if (err.code == 'unimplemented') {
+    } else if (err.code === 'unimplemented') {
       console.error("The current browser does not support offline persistence");
     }
   });
