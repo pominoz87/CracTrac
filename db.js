85b167bd6b46ba55a51985c617ab6f0e97a8cbe9
pominoz87 <ca.stallard@gmail.com>
2025-02-24 22:51:41 +0800

    Add files via upload

--- db.js
+++ db.js
@@ -1,7 +1,5 @@
 // db.js
-
 const db = new Dexie("CrackInspectionDB");
 db.version(1).stores({
-  // Each record contains equipmentID, crackID, coordinates, an array "photos", and a sync flag.
   cracks: "++id, equipmentID, crackID, markerX, markerY, synced"
 });
